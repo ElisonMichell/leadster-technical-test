68da24f7bcbb17c4a6b2e1cf4ce1bd6da28fa590
Elison Michell <dev.elisonmichell@gmail.com>
2023-02-07 00:32:32 -0300

    feat(header): add hero section to header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,27 @@
 import Image from 'next/image'
-import { Container } from './styles'
+import { Logo, HeroSection } from './styles'
 
 import leadster from '/assets/leadster.svg'
+import asset from '/assets/asset-header.svg'
 
 export function Header() {
 	return(
-		<Container>
-			<Image src={leadster} alt="Leadster logo" />
-		</Container>
+		<header>
+			<Logo>
+				<Image src={leadster} alt="Leadster logo" />
+			</Logo>
+			<HeroSection>
+				<div>
+					<h1>
+						Menos Conversinha,<br/>
+						<span>
+							Mais Conversão
+							<Image src={asset} alt="" />
+						</span>
+					</h1>
+					<h2>Conheça as estratégias que <b>mudaram o jogo</b> e como aplicá-las no seu negócio</h2>
+				</div>
+			</HeroSection>
+		</header>
 	)
 }
--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.header`
+export const Logo = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -10,5 +10,51 @@ export const Container = styled.header`
 	img {
 			height: 2.25rem;
 			width: auto;
+	}
+`
+
+export const HeroSection = styled.div`
+	background: ${props => props.theme.colors.surface};
+	padding: 11rem 0;
+
+	div {
+		width: max-content;
+		margin: auto;
+		text-align: center;
+
+		h1 {
+			padding: 1rem 2rem;
+			position: relative;
+			font-weight: 500;
+			font-size: 3rem;
+
+			::after {
+				content: "";
+				position: absolute;
+				width: 100%;
+				height: 1px;
+				bottom: 0;
+				left: 0;
+				background: ${props => props.theme.colors.border};
+			}
+		}
+
+		span {
+			position: relative;
+			color: ${props => props.theme.colors.primary};
+			font-size: 5.5rem;
+			font-weight: bold;
+
+			img {
+				position: absolute;
+				right: -20px;
+				top: 14px;
+			}
+		}
+
+		h2 {
+			margin-top: 1.25rem;
+			font-size: 1.125rem;
+		}
 	}
 `

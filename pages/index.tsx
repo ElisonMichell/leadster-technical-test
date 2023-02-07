9fe8d22f9283a5542b3479fed46a2291fa6a1869
Elison Michell <dev.elisonmichell@gmail.com>
2023-02-06 22:26:56 -0300

    feat(home): add header component to page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Plus_Jakarta_Sans } from '@next/font/google'
+import { Header } from '@/components/Header'
 
 const font = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={font.className}>
+        <Header />
       </main>
     </>
   )

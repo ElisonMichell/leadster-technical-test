ed6573a31f0ed02e21f0b62befb57b82e8bad33c
Elison Michell <dev.elisonmichell@gmail.com>
2023-02-08 12:09:46 -0300

    feat(components): add lead banner component

---
+++ components/LeadBanner/index.tsx
@@ -0,0 +1,37 @@
+import { Container, Chart, Content } from './styles'
+import Image from 'next/image'
+
+import chart from '/assets/chart.png'
+import selo from '/assets/selo.png'
+import card from '/assets/card.svg'
+import star from '/assets/stars.svg'
+import { Button } from '../Button'
+
+export function LeadBanner() {
+  return(
+    <Container>
+    <Chart>
+      <Image src={chart} alt='leadster chart' unoptimized/>
+    </Chart>
+    <Content>
+      <div className='title'>
+        <h3>
+          Pronto para triplicar sua<br/>
+          <b>Geração de Leads?</b>
+        </h3><br/>
+        <p>Criação e ativação em <b>4 minutos.</b></p>
+      </div>
+      <div className='line-content'>
+        <Button title='VER DEMONSTRAÇÃO' rounded variant='primary'/>
+        <Image src={selo} alt="selo RD Station" />
+      </div>
+      <div className='footer-content'>
+        <Image src={card} alt='card icon'/>
+        <p><b>Não</b> é necessário Cartão de Crédito | </p>
+        <Image src={star} alt='star icon'/>
+        <p><b>4.9</b>/5 média de satisfação</p>
+      </div>
+    </Content>
+  </Container>
+  )
+}
---
+++ components/LeadBanner/styles.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+  background: ${props => props.theme.colors.surface};
+	padding: 4rem;
+`
+
+export const Chart = styled.div`
+  flex: 1;
+
+  img {
+    max-width: 739px;
+    width: 100%;
+    height: auto;
+  }
+`
+
+export const Content = styled.div`
+  .title {
+    position: relative;
+    padding-bottom: 1.25rem;
+    margin-bottom: 1rem;
+
+    ::after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      bottom: 0;
+      left: 0;
+      background: ${props => props.theme.colors.border};
+    }
+
+    h3 {
+      font-size: 2.625rem;
+      font-weight: 500;
+      line-height: 3rem;
+      white-space: nowrap;
+    }
+
+    p {
+      font-size: 1.5rem;
+    }
+  }
+
+  .line-content {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .footer-content {
+    display: flex;
+    gap: 0.25rem;
+    margin-top: 1rem;
+    font-size: 15px;
+    font-weight: 600;
+    align-items: center;
+  }
+`
